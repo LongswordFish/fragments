2c8f99aedfd587e3c07ff9d78090c941674a58b4
longswordfish <robinyu9840@gmail.com>
2022-01-12 14:18:39 -0500

    app.js created

---
+++ src/app.js
@@ -0,0 +1,86 @@
+// src/app.js
+
+//express as the core 
+const express = require('express');
+//Cross-Origin Resource Sharing so it could visit resources in other origins
+const cors = require('cors');
+//secure your Express apps by setting various HTTP headers.
+const helmet = require('helmet');
+//compress the respond bodys
+const compression = require('compression');
+
+// Version from our package.json file
+const { version } = require('../package.json');
+
+const logger = require('./logger');
+const pino = require('pino-http')({
+  // Use our default logger instance, which is already configured
+  logger,
+});
+
+// Create an express app instance we can use to attach middleware and HTTP routes
+const app = express();
+
+// Use logging middleware
+app.use(pino);
+
+// Use security middleware
+app.use(helmet());
+
+// Use CORS middleware so we can make requests across origins
+app.use(cors());
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Define a simple health check route. If the server is running
+// we'll respond with a 200 OK.  If not, the server isn't healthy.
+app.get('/', (req, res) => {
+  // Clients shouldn't cache this response (always request it fresh)
+  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
+  res.setHeader('Cache-Control', 'no-cache');
+
+  // Send a 200 'OK' response with info about our repo
+  res.status(200).json({
+    status: 'ok',
+    author: 'Jianchang Yu',
+    githubUrl: 'https://github.com/LongswordFish/fragments',
+    version,
+  });
+});
+
+// Add 404 middleware to handle any requests for resources that can't be found can't be found
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'error',
+    error: {
+      message: 'not found',
+      code: 404,
+    },
+  });
+});
+
+// Add error-handling middleware to deal with anything else
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // We may already have an error response we can use, but if not, use a generic
+  // 500 server error and message.
+  const status = err.status || 500;
+  const message = err.message || 'unable to process request';
+
+  // If this is a server error, log something so we can see what's going on.
+  if (status > 499) {
+    logger.error({ err }, `Error processing request`);
+  }
+
+  res.status(status).json({
+    status: 'error',
+    error: {
+      message,
+      code: status,
+    },
+  });
+});
+
+// Export our `app` so we can access it in server.js
+module.exports = app;
