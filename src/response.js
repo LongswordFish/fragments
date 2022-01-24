35dd3ff20ec4f6e2642c55dc76d0d9a338292f41
LongswordFish <robinyu9840@gmail.com>
2022-01-24 12:10:54 -0500

    fix the two failed tests

--- src/response.js
+++ src/response.js
@@ -11,9 +11,12 @@
 module.exports.createSuccessResponse = function (data) {
   return {
     status: 'ok',
-    // TODO ...
+    // Use the spread operator to clone `data` into our object, see:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+    ...data,
   };
 };
+
 
 /**
  * An error response looks like:
@@ -27,5 +30,11 @@ module.exports.createSuccessResponse = function (data) {
  * }
  */
 module.exports.createErrorResponse = function (code, message) {
-  // TODO ...
+  return {
+    "status": "error",
+    "error": {
+      code,
+      message,
+    }
+  }
 };
